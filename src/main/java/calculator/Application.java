2ebc539599c9731b554f57c70b2a916b4407094e
gakgya <mirejiho@naver.com>
2025-10-16 01:32:50 +0900

    feat(Application.java): 문자열 입력시 덧셈하는 기능추가
    입력된 문자열 에서 , 와 : 를 기준으로 구분하여 생긴숫자 덧셈하는 기능추가
    
    calculator #1

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import java.util.*;
+
+import camp.nextstep.edu.missionutils.*;
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String a = "";
+        int result = 0;
+        System.out.println("덧셈할 문자열을 입력해주세요");
+        a = Console.readLine();
+        int[] intArray = Arrays.stream(a.split(",|:")).mapToInt(Integer::parseInt).toArray();
+        for (int i : intArray) {
+            result += i;
+        }
+        System.out.println("결과 : " + result);
     }
 }
