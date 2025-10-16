401c565d521f1d0a8a1269e9277fba73eae57187
gakgya <mirejiho@naver.com>
2025-10-17 01:53:46 +0900

    feat(Application.java): 입력받은 문자열중 커스텀 구분자 지정기능 추가
    "//" 와 "\n" 사이에 특정 문자가 들어있을경우 커스텀 구분자로 사용하겨 문자열 의 덧셈에 사용하도록 기능추가
    
    calculator #2

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 import camp.nextstep.edu.missionutils.*;
 
@@ -8,14 +9,41 @@ import camp.nextstep.edu.missionutils.*;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String a = "";
+        String inputText = "";
+        String separator = null;
         int result = 0;
+        String regex = ",|:";
         System.out.println("덧셈할 문자열을 입력해주세요");
-        a = Console.readLine();
-        int[] intArray = Arrays.stream(a.split(",|:")).mapToInt(Integer::parseInt).toArray();
+        inputText = Console.readLine();
+        // String temp = customDetect(inputText);
+        if(customDetect(inputText)){
+            separator = takeSeparator(inputText);
+            inputText = inputText.split("\\\\n")[1];
+            regex += "|" + Pattern.quote(separator);
+        }
+        
+        int[] intArray = Arrays.stream(inputText.split(regex)).mapToInt(Integer::parseInt).toArray();
         for (int i : intArray) {
             result += i;
         }
         System.out.println("결과 : " + result);
+    }
+
+    private static Boolean customDetect(String test){
+        try {
+            if (test.indexOf("//") != -1 && test.indexOf("\\n") != -1) {
+                return true;
+            }else{
+                return false;
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            return null;
+        }
+    }
+    private static String takeSeparator(String test){
+        int tempFront = test.indexOf("//") + 2;
+        int tempBack = test.indexOf("\\n");
+        return test.substring(tempFront, tempBack);
     }
 }
