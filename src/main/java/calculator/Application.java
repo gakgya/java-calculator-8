602c57e4608a1c8e64c4964eebc405b1b5a3a8eb
gakgya <mirejiho@naver.com>
2025-10-18 01:01:56 +0900

    feat(Application.java): 예외처리 추가
    자바 시스템상 입력불가능한 문자 , 커스텀 문자 비어있음 , 잘못된 구분자 사용 , 커스텀 구분자에 숫자사용 , 커스텀 구분자 비어있음 에 예외처리 추가
    
    calculator #3

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,41 +9,64 @@ import camp.nextstep.edu.missionutils.*;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String inputText = "";
-        String separator = null;
-        int result = 0;
-        String regex = ",|:";
-        System.out.println("덧셈할 문자열을 입력해주세요");
-        inputText = Console.readLine();
-        // String temp = customDetect(inputText);
-        if(customDetect(inputText)){
-            separator = takeSeparator(inputText);
-            inputText = inputText.split("\\\\n")[1];
-            regex += "|" + Pattern.quote(separator);
+        Boolean condition = true;
+        while (condition) {
+            try {
+                String inputText = "";
+                String separator = null;
+                int result = 0;
+                String regex = ",|:";
+                System.out.println("덧셈할 문자열을 입력해주세요");
+
+                inputText = Console.readLine();
+                if(inputText.contains("//") && inputText.contains("\\n")){
+                    separator = takeSeparator(inputText);
+                    textConfirm(inputText,separator);
+                    inputText = inputText.split("\\\\n")[1];
+                    regex += "|" + Pattern.quote(separator);
+                }
+                
+                int[] intArray = Arrays.stream(inputText.split(regex)).mapToInt(Integer::parseInt).toArray();
+                for (int i : intArray) {
+                    result += i;
+                }
+                System.out.println("결과 : " + result);
+            } catch (Exception e) {
+                // TODO: handle exception
+                stopProgram(e.getMessage());
+            }
+            
         }
-        
-        int[] intArray = Arrays.stream(inputText.split(regex)).mapToInt(Integer::parseInt).toArray();
-        for (int i : intArray) {
-            result += i;
-        }
-        System.out.println("결과 : " + result);
+       
     }
 
-    private static Boolean customDetect(String test){
-        try {
-            if (test.indexOf("//") != -1 && test.indexOf("\\n") != -1) {
-                return true;
-            }else{
-                return false;
-            }
-        } catch (Exception e) {
-            // TODO: handle exception
-            return null;
+    // private static Boolean customDetect(String test){
+    //     return test.contains("//") && test.contains("\\n");
+    // }
+    private static String takeSeparator(String text){
+        String temp;
+        int tempFront = text.indexOf("//") + 2;
+        int tempBack = text.indexOf("\\n");
+        temp = text.substring(tempFront, tempBack);
+        if(temp.matches("[+-]?\\d*(\\.\\d+)?"));{
+            stopProgram("custom separator can not use number!");
+        }
+        return text.substring(tempFront, tempBack);
+    }
+
+    private static void textConfirm(String text,String separator){
+        String pattern1 = String.format("^//.+(?:\\\\n)\\d+(?:[,:%s]\\d+)*$",separator);
+        String pattern2 = "^\\d+[,:]\\d+(?:[,:]\\d+)*$";
+        if(separator == null){
+            stopProgram("separator cannot be null!");
+        }
+        if(!text.matches(pattern1) && !text.matches(pattern2)){
+            stopProgram("separator is invalid!");
         }
     }
-    private static String takeSeparator(String test){
-        int tempFront = test.indexOf("//") + 2;
-        int tempBack = test.indexOf("\\n");
-        return test.substring(tempFront, tempBack);
+
+    private static void stopProgram(String errMessage){
+        throw new IllegalArgumentException(errMessage);
     }
+    
 }
