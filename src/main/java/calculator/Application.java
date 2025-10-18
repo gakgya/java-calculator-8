a3fa270d7e7d159b4de720d42dfc74dfde35aecb
gakgya <mirejiho@naver.com>
2025-10-19 00:28:17 +0900

    style(Application.java): 수정된 코드 스타일에 맞게 수정 및 에러코드 한글로 변경
    gradle테스트에 통과되도록 수정된 코드 스타일에 맞게 수정 및 에러코드 한글로 변경
    
    calculator #7

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,40 +9,40 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        boolean condition = true;
 
-            try {
-                String inputText = "";
-                String separator = null;
-                BigDecimal result = BigDecimal.ZERO;
-                String regex = ",|:";
+        try {
+            String inputText = "";
+            String separator = null;
+            BigDecimal result = BigDecimal.ZERO;
+            String regex = ",|:";
 
-                System.out.println("덧셈할 문자열을 입력해주세요");
-                inputText = Console.readLine();
+            System.out.println("덧셈할 문자열을 입력해주세요");
+            inputText = Console.readLine();
 
-                if (inputText.equals("")) {
-                    System.out.println("결과 : 0");
-                }
-
-                if (inputText.contains("//") && inputText.contains("\\n")) {
-                    separator = takeSeparator(inputText);
-                    textConfirm(inputText, separator);
-                    inputText = inputText.split("\\\\n")[1];
-                    regex += "|" + Pattern.quote(separator);
-                }
-
-                for (String i : inputText.split(regex)) {
-                    if(i.contains("-")){
-                        stopProgram("음수 사용불가!");
-                    }
-                    result = result.add(new BigDecimal(i));
-                }
-
-                System.out.println("결과 : " + result);
-            } catch (Exception e) {
-                stopProgram(e.getMessage());
+            if (inputText.equals("")) {
+                System.out.println("결과 : 0");
+                return;
             }
+
+            if (inputText.contains("//") && inputText.contains("\\n")) {
+                separator = takeSeparator(inputText);
+                textConfirm(inputText, separator);
+                inputText = inputText.split("\\\\n")[1];
+                regex += "|" + Pattern.quote(separator);
+            }
+
+            for (String i : inputText.split(regex)) {
+                if (i.contains("-")) {
+                    stopProgram("음수 사용불가!");
+                }
+                result = result.add(new BigDecimal(i));
+            }
+
+            System.out.println("결과 : " + result);
+        } catch (Exception e) {
+            stopProgram(e.getMessage());
         }
+    }
 
     private static String takeSeparator(String text) {
         int tempFront = text.indexOf("//") + 2;
@@ -50,7 +50,7 @@ public class Application {
         String temp = text.substring(tempFront, tempBack);
 
         if (temp.matches("[+-]?\\d*(\\.\\d+)?")) {
-            stopProgram("custom separator can not use number!");
+            stopProgram("구분자 는 숫자사용이 불가합니다.");
         }
 
         return text.substring(tempFront, tempBack);
@@ -61,9 +61,9 @@ public class Application {
         String pattern2 = "^\\d+[,:]\\d+(?:[,:]\\d+)*$";
 
         if (separator == null) {
-            stopProgram("separator cannot be null!");
+            stopProgram("커스텀 구분자 입력시 필수 입력해야합니다!");
         } else if (!text.matches(pattern1) && !text.matches(pattern2)) {
-            stopProgram("separator is invalid!");
+            stopProgram("올바르지 않은 구분자입니다.");
         }
     }
 
