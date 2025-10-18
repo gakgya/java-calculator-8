662dec630749d68c375880e0826c9f58e8aa5e95
gakgya <mirejiho@naver.com>
2025-10-18 21:46:27 +0900

    fix(Application.java): 소수입력시 게산안되는 버그 수정
    정수입력만 계산되는 버그 수정및 부동소수점과 정수의 계산시 자바에서 생기는 오차수정
    
    calculator #5

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,7 @@ package calculator;
 
 import java.util.*;
 import java.util.regex.Pattern;
+import java.math.BigDecimal;
 
 import camp.nextstep.edu.missionutils.*;
 
@@ -14,7 +15,7 @@ public class Application {
             try {
                 String inputText = "";
                 String separator = null;
-                int result = 0;
+                BigDecimal result = BigDecimal.ZERO;
                 String regex = ",|:";
                 System.out.println("덧셈할 문자열을 입력해주세요");
 
@@ -26,9 +27,8 @@ public class Application {
                     regex += "|" + Pattern.quote(separator);
                 }
                 
-                int[] intArray = Arrays.stream(inputText.split(regex)).mapToInt(Integer::parseInt).toArray();
-                for (int i : intArray) {
-                    result += i;
+                for (String i : inputText.split(regex)) {
+                    result = result.add(new BigDecimal(i));
                 }
                 System.out.println("결과 : " + result);
             } catch (Exception e) {
@@ -40,15 +40,12 @@ public class Application {
        
     }
 
-    // private static Boolean customDetect(String test){
-    //     return test.contains("//") && test.contains("\\n");
-    // }
     private static String takeSeparator(String text){
         String temp;
         int tempFront = text.indexOf("//") + 2;
         int tempBack = text.indexOf("\\n");
         temp = text.substring(tempFront, tempBack);
-        if(temp.matches("[+-]?\\d*(\\.\\d+)?"));{
+        if(temp.matches("[+-]?\\d*(\\.\\d+)?")){
             stopProgram("custom separator can not use number!");
         }
         return text.substring(tempFront, tempBack);
@@ -59,8 +56,7 @@ public class Application {
         String pattern2 = "^\\d+[,:]\\d+(?:[,:]\\d+)*$";
         if(separator == null){
             stopProgram("separator cannot be null!");
-        }
-        if(!text.matches(pattern1) && !text.matches(pattern2)){
+        }else if(!text.matches(pattern1) && !text.matches(pattern2)){
             stopProgram("separator is invalid!");
         }
     }
